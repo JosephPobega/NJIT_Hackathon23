479aaeaf363f21d366f79a35e09fb2c1f96dfb4c
Vanderbilt <122231587+Vannndy@users.noreply.github.com>
2023-11-04 16:21:56 -0400

    Updated Wheel
    Now functional, not finished, but it spins!

--- scripts/wheel.js
+++ scripts/wheel.js
@@ -2,8 +2,8 @@ let wheel = document.querySelector(".wheel");
 let spinButton = document.querySelector("button");
 
 function spinWheel() {
-    let randomDegree = Math.floor(Math.random() * 360);
-    wheel.style.transform = `rotate(${randomDegree}deg)`;
+    let randomAngle = 360 * (4 + Math.floor(Math.random() * 4)); // Rotate multiple times
+    wheel.style.transform = `rotate(${randomAngle}deg)`;
 }
 
 spinButton.addEventListener("click", spinWheel);
